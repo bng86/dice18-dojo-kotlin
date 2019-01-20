521ff5df67369271d065b45766835525c886f0b6
andyang <yang.bng86@gmail.com>
2019-01-20 09:30:34 +0800

    init commit

---
+++ app/src/main/java/andynag/tw/dice18/MainActivity.kt
@@ -0,0 +1,12 @@
+package andynag.tw.dice18
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
