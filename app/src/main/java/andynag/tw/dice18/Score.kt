4be33d90ecf6389f0fc676328ea2745db4cd9a68
andyang <yang.bng86@gmail.com>
2019-01-23 20:56:20 +0800

    Lance

--- app/src/main/java/andynag/tw/dice18/Score.kt
+++ app/src/main/java/andynag/tw/dice18/Score.kt
@@ -7,21 +7,24 @@ class Score {
         const val DICE_17 = 18
 
         fun value(dices: List<Dice>): Int {
-            val distinctList = dices.distinct()
-            val isOneColor = (distinctList.size == 1 )
-            if(isOneColor){
-                return dices[0].number + DICE_17
-            }
-            val is18la = (distinctList.size == 2) &&
-                    dices.contains(Dice(6))
-            if (is18la)
-                return DICE_17
+
+                return when(dices.distinct().size) {
+                    1 -> return dices[0].number + DICE_17
+                    2 -> {
+                        if(dices.contains(Dice(6))) {
+                            return DICE_17
+                        } else {
+                            return -1
+                        }
+
+                    } else -> return -1
+
+                }
+
+
 
             // TODO please finish this
-            println(dices.groupBy {
-                it.number
-            })
-            return 0
+
         }
 
         private fun getDistinct(dices: List<Dice>) = dices.distinct()
